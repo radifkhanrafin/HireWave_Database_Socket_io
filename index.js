88767e7b65c4301d57e350b0c7042d686601bd7b
radifkhanrafin <meherafkabir@gmail.com>
2023-09-17 20:28:39 +0600

    complete user connect route

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const { Server } = require("socket.io");
 
-const io = new Server({ cors:" http://localhost:5173" });
+const io = new Server({ cors: "http://localhost:5173" });
+
+let onlineUsers=[];
 
 io.on("connection", (socket) => {
- console.log("new collection" , socket.id)
+    console.log("new collection", socket.id)
+
+    socket.on("addNewUsers", (userId) => {
+        !onlineUsers.some(user => user.userId === userId) &&
+            onlineUsers.push({
+                userId,
+                socketId: socket.id
+            });
+            console.log("onlineUsers" , onlineUsers)
+
+            io.emit("getOnlineUsers" , onlineUsers)
+    });
+    io.emit("disconnect" , ()=>{
+        onlineUsers=onlineUsers.filter((user)=>user.socketId !== socket.id);
+        io.emit("getOnlineUsers" , onlineUsers)
+    })
+
 });
 
 io.listen(3000);
