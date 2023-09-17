994c8ead89922d70b81fd5ed3bab6830def5b558
radifkhanrafin <meherafkabir@gmail.com>
2023-09-17 18:18:29 +0600

    connect to client side

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { Server } = require("socket.io");
 
-const io = new Server({ /* options */ });
+const io = new Server({ cors:" http://localhost:5173" });
 
 io.on("connection", (socket) => {
-  // ...
+ console.log("new collection" , socket.id)
 });
 
 io.listen(3000);
