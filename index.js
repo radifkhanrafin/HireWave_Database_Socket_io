1503669446cadee9d9a20e67e640063ff501c529
radifkhanrafin <meherafkabir@gmail.com>
2023-09-18 12:01:33 +0600

    complete real time chat

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Server } = require("socket.io");
 
 const io = new Server({ cors: "http://localhost:5173" });
 
-let onlineUsers=[];
+let onlineUsers = [];
 
 io.on("connection", (socket) => {
     console.log("new collection", socket.id)
@@ -13,13 +13,25 @@ io.on("connection", (socket) => {
                 userId,
                 socketId: socket.id
             });
-            console.log("onlineUsers" , onlineUsers)
-
-            io.emit("getOnlineUsers" , onlineUsers)
+        console.log("onlineUsers", onlineUsers)
+ 
+        io.emit("getOnlineUsers", onlineUsers)
     });
-    io.emit("disconnect" , ()=>{
-        onlineUsers=onlineUsers.filter((user)=>user.socketId !== socket.id);
-        io.emit("getOnlineUsers" , onlineUsers)
+
+    // add message 
+    socket.on("sendMessage", (message) => {
+        console.log("message" , message)
+        const user = onlineUsers.find((user) => user.userId === message.receiver);
+        console.log(onlineUsers)
+        console.log("user" ,user)
+        if (user) {
+            io.to(user.socketId).emit("getMessages", message)
+        }
+    })
+
+    socket.on("disconnect", () => {
+        onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+        io.emit("getOnlineUsers", onlineUsers)
     })
 
 });
